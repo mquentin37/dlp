fa6e1aafa4ea34de13f33cfd414083539ac176cc
Quentin MALANDRE <QMALANDRE@externe.generali.fr>
2018-04-30 16:08:40 +0200

    about skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
 import {AngularFontAwesomeModule} from 'angular-font-awesome';
 import {HomeComponent} from './components/home/home.component';
 import {appModuleRouter} from './app.routes';
-import { AboutComponent } from './components/about/about.component';
-import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import {AboutComponent} from './components/about/about.component';
+import {PortfolioComponent} from './components/portfolio/portfolio.component';
 
 
 @NgModule({
