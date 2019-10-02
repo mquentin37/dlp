7c83bf3fded0de3cc62eaef286e22739dade7068
Quentin Malandre <malandrequentin@gmail.com>
2019-10-02 14:44:42 +0200

    Experiences component

---
+++ src/app/components/experiences/experiences.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {RouteEnum, RoutingService} from '../../service/routing.service';
+
+@Component({
+  selector: 'app-experiences',
+  templateUrl: './experiences.component.html',
+  styleUrls: ['./experiences.component.scss']
+})
+export class ExperiencesComponent implements OnInit {
+
+  collapsed_Zenika = true;
+  collapsed_Apside = true;
+  collapsed_Nokia = true;
+
+  zenikaCard = {
+    srcImage: '',
+    title: 'TLS / SSL Certificates Management - Spring API',
+    description: '',
+    tags: ['Security'],
+    technologies: [`<img src="../../../assets/logos/spring.svg" height="20px"/>`]
+  };
+
+  constructor(private routingService: RoutingService) { }
+
+  ngOnInit() {
+    this.routingService.route = RouteEnum.EXPERIENCE;
+  }
+
+}
